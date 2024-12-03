b5f4ccde1ac140f36a7206daec67cf9d0e038b2c
David Shabelian <david.shabelian@joy-dev.com>
2024-12-03 14:27:44 +0300

    chore: added babel, and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -40,5 +40,25 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
